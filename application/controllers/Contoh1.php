c14cb1d9d8ae648195ead03dfa018c562a65041e
megaoktafia <megaokta13@gmail.com>
2021-09-30 16:57:38 +0700

    upload awal

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Mega Oktafiyanti 
+        Saya tinggal di daerah Cileungsi
+        Hobi saya adalah membaca buku";
+    }
+}
