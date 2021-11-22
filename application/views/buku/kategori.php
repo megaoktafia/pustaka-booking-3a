5398ae1c9f25bb8bda07fc5412e5aee7ae95dd8c
megaoktafia <megaokta13@gmail.com>
2021-11-22 14:18:31 +0700

    P11

--- application/views/buku/kategori.php
+++ application/views/buku/kategori.php
@@ -45,12 +45,12 @@
 <!-- End of Main Content -->
 
 <!-- Modal Tambah kategori baru-->
-<div class="modal fade" id="kategoriBaruModal" tabindex="-1"role="dialog" aria-labelledby="kategoriBaruModalLabel" ariahidden="true">
+<div class="modal fade" id="kategoriBaruModal" tabindex="-1"role="dialog" aria-labelledby="kategoriBaruModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title"id="kategoriBaruModalLabel">Tambah Kategori</h5>
-                <button type="button" class="close" datadismiss="modal" aria-label="Close">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
                 </button>
             </div>
