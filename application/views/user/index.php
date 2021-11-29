42d6ded5eacb05cd8a57f919078d0390e824bfe1
megaoktafia <megaokta13@gmail.com>
2021-11-29 12:33:31 +0700

    P

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -1,6 +1,6 @@
 <!-- Begin Page Content -->
 <div class="container-fluid">
-    
+ 
     <div class="row">
         <div class="col-lg-6 justify-content-x">
             <?= $this->session->flashdata('pesan'); ?>
@@ -15,12 +15,13 @@
                 <div class="card-body">
                     <h5 class="card-title"><?= $user['nama']; ?></h5>
                     <p class="card-text"><?= $user['email']; ?></p>
-                    <p class="card-text"><small class="textmuted">Jadi member sejak: <br><b><?= date('d F Y', $user['tanggal_input']); ?></b></small></p>
+                    <p class="card-text"><small class="textmuted">Jadi member sejak: <br><b><?= date('d F Y', $user['tanggal_input']); ?></b></small></p>
                 </div>
                 <div class="btn btn-info ml-3 my-3">
-                    <a href="<?= base_url('user/ubahprofil'); ?>"class="text text-white"><i class="fas fa-user-edit"></i> Ubah Profil</a>
+                    <a href="<?= base_url('user/ubahprofil'); ?>" class="text text-white"><i class="fas fa-user-edit"></i> Ubah Profil</a>
                 </div>
             </div>
+        
         </div>
     </div>
 
